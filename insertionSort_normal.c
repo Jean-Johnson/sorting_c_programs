64bb696fd8c3ff9433e398d304f05bff63d43904
Jean-Johnson <jeanjohnsonldrs@gmail.com>
2019-07-13 20:56:39 +0530

    insertion sort completed

---
+++ insertionSort_normal.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void insertSort(int ar[],int n){
+	int i,j,k;
+	for(i=1;i<n;i++){
+		k=ar[i];
+		j=i-1;
+		while(j>=0 && ar[j]>k){
+			ar[j+1]=ar[j];
+			j--;
+		}
+		ar[j+1]=k;
+	}
+}
+void main(){
+	int i,n,ar[1000];
+	printf("[+]Enter the number of elements(<1000) >> ");
+	scanf("%d",&n);
+	printf("[+]Enter the number >>\n");
+	for(i=0;i<n;i++)
+		scanf("%d",&ar[i]);
+	insertSort(ar,n);
+	for(i=0;i<n;i++)
+		printf("%d ",ar[i]);
+}
